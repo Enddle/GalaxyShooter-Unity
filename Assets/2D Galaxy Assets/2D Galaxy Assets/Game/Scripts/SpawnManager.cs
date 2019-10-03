0772cf47df1d5830fd672aba512627486b60ea64
Enddle <Enddle@users.noreply.github.com>
2019-10-02 23:57:09 -0400

    1002
    - Add SpawnManager
    - Fix Triple/Max Shot

---
+++ Assets/2D Galaxy Assets/2D Galaxy Assets/Game/Scripts/SpawnManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    [SerializeField] private GameObject EnemyPrefab = null;
+    [SerializeField] private GameObject[] PowerupPrefabs = null;
+        // [] means an array
+
+    // Start is called before the first frame update
+    void Start() {
+        
+        StartCoroutine(EnemySpawnRoutine());
+
+        StartCoroutine(PowerupSpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private IEnumerator EnemySpawnRoutine() {
+
+        while (true) {
+
+            Instantiate(EnemyPrefab, new Vector3(Random.Range(-7.5f, 7.5f), 6.5f, 0f), Quaternion.identity);
+            Instantiate(EnemyPrefab, new Vector3(Random.Range(-7.5f, 7.5f), 6.5f, 0f), Quaternion.identity);
+
+            yield return new WaitForSeconds(3.0f);
+        }
+    }
+
+    private IEnumerator PowerupSpawnRoutine() {
+
+        while (true) {
+
+            int r = Random.Range(0,4);
+
+            Instantiate(PowerupPrefabs[r], new Vector3(Random.Range(-7.5f, 7.5f), 6.5f, 0f), Quaternion.identity);
+
+            yield return new WaitForSeconds(10.0f);
+        }
+    }
+
+}
